4a5cd4f25e41e848e0036f6330ac1dde387a4a79
Emanuel Ureña <eurena@soin.co.cr>
2021-09-20 08:38:43 -0600

    initial commit

---
+++ src/main/java/com/soinlabs/soapmocking/SoapMockingApplication.java
@@ -0,0 +1,13 @@
+package com.soinlabs.soapmocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapMockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapMockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soinlabs/soapmocking/SoapMockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soinlabs.soapmocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapMockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
